0c2dc6fce4138a1e7039721e067e047ab1e473e5
rsouvik <sales@getmypickly.com>
2025-02-19 09:48:12 -0800

    Did some refactoring and added tests etc.

--- lib/conjured.ex
+++ lib/conjured.ex
@@ -2,6 +2,7 @@ defmodule Conjured do
   @moduledoc """
   implements Conjured Behaviour
   """
+  import Helper, only: [update_item: 2]
 
   @behaviour Base_Common
   def update(item), do: update_item(item, 2)
